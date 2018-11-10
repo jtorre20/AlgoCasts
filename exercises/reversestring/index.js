d9fa36b05b8caba3fd569169b4e79226ee998105
Julio <jtorres1989@gmail.com>
2018-11-09 19:38:28 -0500

    added solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,38 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Using built in methods(split, reverse, and join)
+//****************** */
+
+function reverse(str) {
+  // let splitStr = str.split('');
+  // let reversed = splitStr.reverse().join('');
+  // return reversed;
+  return str.split('').reverse().join('')
+}
+
+//Using a decrementing for loop
+//****************** */
+function reverse(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--){
+    reversed += str[i]
+  }
+  return reversed
+}
+
+//****************** */
+// const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+function reverse(str) {
+
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
+
 
 module.exports = reverse;
