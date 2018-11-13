11971e89067e6f6af6c10b5756d23aeb745c2b09
Julio <jtorres1989@gmail.com>
2018-11-12 21:17:20 -0500

    added pascal and maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+  let chars = {};
+  let maxKey = '';
+  let max = 0
+
+  for (let char of str) {
+    if (!chars[char]) {
+      chars[char] = 1
+    } else {
+      chars[char]+=1
+    }
+}
+
+for (let char in chars){
+  if (chars[char] > max){
+    max = chars[char]
+    maxKey = char
+  }
+}
+return maxKey
+}
+
+maxChar('Hello there')
 
 module.exports = maxChar;
